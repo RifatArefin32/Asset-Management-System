195ca89d13c4c99dbf3510daa9375e72031ebb0a
Rifat Arefin <arefinrifat786@gmail.com>
2024-04-03 22:06:24 +0600

    Upload All

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.3.4',
+  'full_app_version' => 'v6.3.4 - build 13139-g6f9ba6ede',
+  'build_version' => '13139',
+  'prerelease_version' => '',
+  'hash_version' => 'g6f9ba6ede',
+  'full_hash' => 'v6.3.4-234-g6f9ba6ede',
+  'branch' => 'master',
+);
